3b006de8f2ab2c6c0047276defeffb408364917d
Yohan Garcia <yoanvaldes03@gmail.com>
2024-01-19 23:06:52 -0500

    Initial commit

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="row g-0 d-flex justify-content-center mt-5">
+    <div class="col-md-12 tex-center">
+      <p class="lead fw-bold">About Us</p>
+    </div>
+    <div class="col-md-6 text-start">
+      <p>
+        Here at Laz Appliance Repair, where exceptional service meets unmatched expertise in home appliance repairs.
+         With a passion for precision and a commitment to customer satisfaction, we take pride in being your trusted 
+         partner for all your appliance repair needs. Our dedicated team of skilled technicians brings years of experience 
+         to the table, ensuring efficient and reliable solutions for a wide range of home appliances. Whether it's a 
+         malfunctioning refrigerator, a misbehaving washing machine, or any other household appliance, we understand 
+         the importance of a smoothly running home. At Laz Appliance Repair, we strive to exceed expectations by delivering 
+         prompt, professional, and affordable repair services. Your comfort and convenience are our top priorities, 
+         and we look forward to the opportunity to restore your appliances to their optimal functionality. Choose 
+         Laz Appliance Repair for excellence in home 
+        appliance repairs – where we turn challenges into solutions for a happier and more smoothly running home.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container-fluid p-0">
+    <div class="row g-0">
+      <div class="col-md-12">
+        <div class="position-relative">
+          <img src="../assets/heroImg.png" alt="Background Image" class="img-fluid" style="object-fit: cover; width: 100%; height: 100vh;">
+          <div class="overlay"></div>
+          <div class="position-absolute top-50 start-50 translate-middle text-white">
+            <h1 class="display-4 fw-bold">Don't ditch it. Switch to fix it!</h1>
+            <a href="#" class="btn btn-primary btn-lg text-white">INQUIRE NOW</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
