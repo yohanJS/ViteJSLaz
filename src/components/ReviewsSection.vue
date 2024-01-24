fd2adabf406357d3e2b37e9fd5a9ea53817f2603
Yohan Garcia <yoanvaldes03@gmail.com>
2024-01-24 17:23:23 -0500

    - fixed css issue

--- src/components/ReviewsSection.vue
+++ src/components/ReviewsSection.vue
@@ -39,12 +39,12 @@
               </div>
             </div>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+          <button class="carousel-control-prev mb-5" type="button" data-bs-target="#carouselExampleControls"
             data-bs-slide="prev">
             <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
           </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+          <button class="carousel-control-next mb-5" type="button" data-bs-target="#carouselExampleControls"
             data-bs-slide="next">
             <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
