19339c4b080099b6325008ef5686e1a2e2958f9a
Yohan Garcia <yoanvaldes03@gmail.com>
2024-02-13 13:06:56 -0500

    - added the form capture using axios

--- src/components/ReviewsSection.vue
+++ src/components/ReviewsSection.vue
@@ -26,15 +26,15 @@
                 <path
                   d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708" />
               </svg>
-              <span class="fw-bold"> verified customer</span>
+              <!-- <span class="fw-bold"> verified customer</span> -->
               <div class="stars">
                 <!-- Use the SVG star icon five times for the rating -->
-                <svg v-for="star in 5" :key="star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="none"
+                <!-- <svg v-for="star in 5" :key="star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="none"
                   stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
                   class="star-icon">
                   <polygon
                     points="8 1 10.09 4.26 14 5.27 11 8.64 12.18 11.52 8 9.27 3.82 11.52 5 8.64 1 5.27 5.91 4.26 8 1" />
-                </svg>
+                </svg> -->
               </div>
             </div>
           </div>
