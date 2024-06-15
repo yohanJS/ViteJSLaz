d31531309ea5684df5658cf042d31abe139266c6
Yohan Garcia <yoanvaldes03@gmail.com>
2024-06-15 09:36:37 -0400

    one more try

--- src/components/ReviewsSection.vue
+++ src/components/ReviewsSection.vue
@@ -52,7 +52,7 @@
               </div>
               <img
               v-if="review.rating == '5'"
-                src="../assets/FiveStarts.svg"
+                src="../assets/fiveStarts.svg"
                 class=""
                 width="100"
                 height="100"
