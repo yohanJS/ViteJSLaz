562e7baf8cb570d3baccee5b605c758e5e1a0a51
Yohan Garcia <yoanvaldes03@gmail.com>
2024-06-15 09:20:47 -0400

    added starts icons based on rating

--- src/components/ReviewsSection.vue
+++ src/components/ReviewsSection.vue
@@ -26,15 +26,8 @@
               class="carousel-item"
               :class="{ active: index === 0 }"
             >
-              <img
-                src="../assets/LazLogo.svg"
-                class="rounded-2 shadow-lg p-3 shadow-1-strong"
-                width="100"
-                height="100"
-                alt="Reviewer Avatar"
-              />
-              <p class="m-0 p-0 mt-5 w-75 m-auto">{{ review.content }}</p>
-              <p class="lead mt-2">{{ review.author }}</p>
+            <p class="fw-bold lead mt-2">{{ review.author }}</p>
+              <p class="m-0 p-1 mt-3 w-75 m-auto">{{ review.content }}.</p>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -57,49 +50,71 @@
                     points="8 1 10.09 4.26 14 5.27 11 8.64 12.18 11.52 8 9.27 3.82 11.52 5 8.64 1 5.27 5.91 4.26 8 1" />
                 </svg> -->
               </div>
+              <img
+              v-if="review.rating == '5'"
+                src="../assets/FiveStarts.svg"
+                class=""
+                width="100"
+                height="100"
+                alt="Reviewer Avatar"
+              />
+              <img
+              v-if="review.rating == '4'"
+                src="../assets/FourStarts.svg"
+                class=""
+                width="100"
+                height="100"
+                alt="Reviewer Avatar"
+              />
+              <img
+              v-if="review.rating == '3'"
+                src="../assets/ThreeStarts.svg"
+                class=""
+                width="100"
+                height="100"
+                alt="Reviewer Avatar"
+              />
+              <img
+              v-if="review.rating == '2'"
+                src="../assets/TwoStarts.svg"
+                class=""
+                width="100"
+                height="100"
+                alt="Reviewer Avatar"
+              />
+              <img
+              v-if="review.rating == '1'"
+                src="../assets/OneStart.svg"
+                class=""
+                width="100"
+                height="100"
+                alt="Reviewer Avatar"
+              />
             </div>
           </div>
           <button
-            class="carousel-control-prev mb-5"
+            class="carousel-control-prev"
             type="button"
             data-bs-target="#reviewsCarousel"
             data-bs-slide="prev"
           >
             <span class="" aria-hidden="true">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                fill="#000000"
-                class="bi bi-arrow-left-circle-fill"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
-                />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000000" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
+</svg>
             </span>
             <span class="visually-hidden">Previous</span>
           </button>
           <button
-            class="carousel-control-next mb-5"
+            class="carousel-control-next"
             type="button"
             data-bs-target="#reviewsCarousel"
             data-bs-slide="next"
           >
             <span class="" aria-hidden="true">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                fill="#000000"
-                class="bi bi-arrow-right-circle-fill"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
-                />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#000000" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/>
+</svg>
             </span>
             <span class="visually-hidden">Next</span>
           </button>
